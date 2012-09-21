baa7deb6d732cf3cbdda2bd88eafb92ce1fd347b
hekt <hektorg@gmail.com>
2012-09-21 22:43:28 +0900

    Modify the layout, use local files

--- scripts/common.js
+++ scripts/common.js
@@ -9,14 +9,30 @@
       .addClass('num');
 
     // resize images
+    // function resizeImages(elem, parent) {
+    //   var parent_width = parent.width()
+    //   elem.each(function() {
+    //     if($(this).width() > parent_width || $(this).hasClass("resized")) {
+    //       $(this)
+    //         .height(parent_width / $(this).width() * $(this).height())
+    //         .width(parent_width)
+    //         .addClass("resized");}});
+    // }
+    // resizeImages($("article img"), $("article"));
+    // $(window).load(function() {
+    //   resizeImages($("article img"), $("article"));
+    // });
+    // $(window).resize(function() {
+    //   resizeImages($("article img"), $("article"));
+    // });
+
+    // Add a class to larger images
     function resizeImages(elem, parent) {
       var parent_width = parent.width()
       elem.each(function() {
-        if($(this).width() > parent_width || $(this).hasClass("resized")) {
+        if($(this).width() > parent_width) {
           $(this)
-            .height(parent_width / $(this).width() * $(this).height())
-            .width(parent_width)
-            .addClass("resized");}});
+            .addClass("large_image");}});
     }
     resizeImages($("article img"), $("article"));
     $(window).load(function() {
