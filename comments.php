7156f4d53d5dd784b4ae5457d418d995f3cb9ca8
hekt <hektorg@gmail.com>
2012-09-22 17:32:24 +0900

    Modify a comment form

--- comments.php
+++ comments.php
@@ -12,66 +12,60 @@
  * @since Twenty Eleven 1.0
  */
 ?>
-	<div id="comments">
-	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyeleven' ); ?></p>
-	</div><!-- #comments -->
+
+<div id="comments">
+  <?php if ( post_password_required() ) : ?>
+  <p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'twentyeleven' ); ?></p>
+  <?php endif; ?>
+
+  <?php if ( have_comments() ) : ?>
+  <h1 id="comments-title">
+    <?php
+	   printf( _n( '%2$s Comment', '%1$s Comments', get_comments_number(), 'twentyeleven' ),
+	   number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' ); ?>
+  </h1>
+
+  <ol class="commentlist">
 	<?php
-			/* Stop the rest of comments.php from being processed,
-			 * but don't kill the script entirely -- we still have
-			 * to fully load the template.
-			 */
-			return;
-		endif;
+	   /* Loop through and list the comments. Tell wp_list_comments()
+	   * to use twentyeleven_comment() to format the comments.
+	   * If you want to overload this in a child theme then you can
+	   * define twentyeleven_comment() and that will be used instead.
+	   * See twentyeleven_comment() in twentyeleven/functions.php for more.
+	   */
+	   wp_list_comments( array( 'callback' => 'twentyeleven_comment' ) );
 	?>
+  </ol>
 
-	<?php // You can start editing here -- including this comment! ?>
-
-	<?php if ( have_comments() ) : ?>
-		<h1 id="comments-title">
-			<?php
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'twentyeleven' ),
-					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
-			?>
-		</h1>
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-above">
-			<h1 class="assistive-text"><?php _e( 'Comment navigation', 'twentyeleven' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentyeleven' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentyeleven' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
-
-		<ol class="commentlist">
-			<?php
-				/* Loop through and list the comments. Tell wp_list_comments()
-				 * to use twentyeleven_comment() to format the comments.
-				 * If you want to overload this in a child theme then you can
-				 * define twentyeleven_comment() and that will be used instead.
-				 * See twentyeleven_comment() in twentyeleven/functions.php for more.
-				 */
-				wp_list_comments( array( 'callback' => 'twentyeleven_comment' ) );
-			?>
-		</ol>
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-		<nav id="comment-nav-below">
-			<h1 class="assistive-text"><?php _e( 'Comment navigation', 'twentyeleven' ); ?></h1>
-			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'twentyeleven' ) ); ?></div>
-			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'twentyeleven' ) ); ?></div>
-		</nav>
-		<?php endif; // check for comment navigation ?>
-
-	<?php
-		/* If there are no comments and comments are closed, let's leave a little note, shall we?
-		 * But we don't want the note on pages or post types that do not support comments.
-		 */
-		elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
+  <?php
+	 elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'twentyeleven' ); ?></p>
-	<?php endif; ?>
+  <p class="nocomments"><?php _e( 'Comments are closed.', 'twentyeleven' ); ?></p>
+  <?php endif; ?>
 
-	<?php comment_form(); ?>
 
+  <?php if ('open' == $post->comment_status): ?>
+  <div id="respond">
+    <h3 id="reply-title">Leave a comment</h3>
+    <form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post" id="commentform">
+      <?php if ($user_ID): ?>
+      <p class="logged-in-as">Logged in as <a href="<?php echo get_option('siteurl'); ?>/wp-admin/profile.php"><?php echo $user_identity; ?></a>. (<a href="<?php echo get_option('siteurl'); ?>/wp-login.php?action=logout">Logout</a>)</p>
+      <?php else: ?>
+      <p class="comment-notes">メールアドレスが公開されることはありません。</p>
+      <p class="comment-form-author"><label for="author" class="inlined">Name</label> <input id="author" name="author" type="text" value="" size="30"></p>
+      <p class="comment-form-email"><label for="email" class="inlined">Email</label> <input id="email" name="email" type="text" value="" size="30"></p>
+      <p class="comment-form-url"><label for="url" class="inlined">Website</label><input id="url" name="url" type="text" value="" size="30"></p>
+      <?php endif; ?>
+
+      <p class="comment-form-comment"><label for="comment">Comment</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea></p>
+      <p class="form-submit">
+        <input name="submit" type="submit" id="submit" value="コメントを送信">
+        <input type='hidden' name='comment_post_ID' value='<?php echo $id; ?>' id='comment_post_ID'>
+        <?php do_action('comment_form', $post->ID); ?>
+      </p>
+    </form>
+  </div>
+
+  <?php endif; ?>
+    
 </div><!-- #comments -->
